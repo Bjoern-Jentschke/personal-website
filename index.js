8fa4b4040aba805f9c4dd195c484c0887e04a0f8
Björn Jentschke <jentschke1@gmail.com>
2024-01-17 09:38:22 +0100

    add eventListener add CSS-basic

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console
+const bodyElement = document.querySelector('[data-js="body"]');
+const lightMode = document.querySelector('[data-js="light-mode-button"]');
+
+lightMode.addEventListener("click", () => {
+    bodyElement.classList.add("button--light");
+})
